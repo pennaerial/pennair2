7623b7c4fe167f08347bf147d5cee90265ed5ac5
Stephen Eyerly <stepheneyerly@gmail.com>
2017-10-10 21:56:45 -0400

    implemented unit conversions

--- src/pennair2/conversions.py
+++ src/pennair2/conversions.py
@@ -1,17 +1,23 @@
-def feet_to_meters():
-    raise NotImplemented
+def feet_to_meters(feet):
+    # One meter is 0.3048
+    return 0.3048 * feet
 
-def meters_to_feet():
-    raise NotImplemented
 
-def miles_to_meters():
-    raise NotImplemented
+def meters_to_feet(meters):
+    return meters / 0.3048
 
-def meters_to_miles():
-    raise NotImplemented
 
-def knots_to_mets():
-    raise NotImplemented
+def miles_to_meters(miles):
+    # One mile is 1609.344 meters
+    return  1609.344 * miles
 
-def mets_to_knots():
-    raise NotImplemented
+
+def meters_to_miles(meters):
+    return meters / 1609.344
+
+
+def knots_to_mets(knots):
+    return 0.514444444444 * knots
+
+def mets_to_knots(mets):
+    return 1.94384449244 * mets
