346a7e0eabe100787e6c9b246e7c6e348f0568de
Stephen Eyerly <stepheneyerly@gmail.com>
2017-10-22 14:36:31 -0400

    created class

--- src/pennair2/gui.py
+++ src/pennair2/gui.py
@@ -8,3 +8,16 @@ except ImportError:
 import PIL.ImageTk
 
 
+class GroundStationApp(Frame):
+    def __init__(self, master=None, TODO=None, DONE=None):
+        super().__init__(master)
+
+
+root = Tk()
+
+w = Label(root, text="Hello, world!")
+w.pack()
+
+root.mainloop()
+
+
