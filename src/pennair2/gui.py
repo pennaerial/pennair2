7c61cdd6d76fe8135638d1a3a3fe3b12305cd23b
Stephen Eyerly <stepheneyerly@gmail.com>
2017-11-05 14:42:34 -0500

    creating framework for loading images

--- src/pennair2/gui.py
+++ src/pennair2/gui.py
@@ -1,28 +1,51 @@
 try:
- # for Python2
- from Tkinter import *
-except ImportError:
  # for Python3
  from tkinter import *
+except ImportError:
+ # for Python2
+ from Tkinter import *
 
-from PIL import ImageTk
-from PIL import Image
+try:
+    from PIL import ImageTk
+    from PIL import Image
+    import googlemaps
+except ImportError:
+    exit()
 
 
 class GroundStationApp:
-    def __init__(self, master):
+    def __init__(self, master, map_width, map_height):
         self.master = master
-        self.load_image()
+        self.initialize_image()
+        self.create_altitude_widget()
+        self.create_speed_widget()
+        self.map_coordinates = (39.9526, 75.1652)
+        self.map_width = map_width
+        self.map_height = map_height
 
-    def load_image(self):
+    def initialize_image(self):
         self.current_image_path = "monkey.jpg"
         self.img = Image.open(self.current_image_path)
         self.photo_img = ImageTk.PhotoImage(self.img)
         self.panel = Label(self.master, image=self.photo_img)
-        self.panel.pack(side="bottom", fill="both", expand="yes")
+        self.panel.grid(row=1, column=0, columnspan=2)
+
+    def create_altitude_widget(self):
+        self.altitude_widget = Label(self.master, text="Altitude")
+        self.altitude_widget.config(font=("Arial", 24))
+        self.altitude_widget.grid(row=0, column=0)
+
+    def create_speed_widget(self):
+        self.altitude_widget = Label(self.master, text="Speed")
+        self.altitude_widget.config(font=("Arial", 24))
+        self.altitude_widget.grid(row=0, column=1)
+
+    def load_image(self):
+        pass
 
     def run(self):
         self.master.mainloop()
+
 
 # The main method which runs the code
 def main():
