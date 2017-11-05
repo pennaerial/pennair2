d51925b053753025589e09e3a8922d452a739dbb
dhruviyer <iyer.dhruv99@gmail.com>
2017-11-05 10:49:43 -0800

    added stubs for get/set servo angle

---
+++ src/pennair2/servo.py
@@ -0,0 +1,15 @@
+
+class Servo:
+
+    def __init__(self, servo_port, servo_min, servo_max, init_angle):
+
+        self.servo_port = servo_port
+        self.servo_min = servo_min
+        self.servo_max = servo_max
+        self.angle = init_angle
+
+    def set_servo_angle(self, angle):
+        self.angle = min(self.servo_max, max(self.servo_min, angle))
+
+    def get_servo_angle(self):
+        return self.angle
