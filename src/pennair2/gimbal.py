20b5cc27e5f20b9359a81934d895bba20a757096
yimengsu <yimengsu@gmail.com>
2017-11-05 13:55:48 -0500

    changes to gimbal and new functions

--- src/pennair2/gimbal.py
+++ src/pennair2/gimbal.py
@@ -1,44 +1,39 @@
+import numpy as np
+import quaternion as qt
+
 class Gimbal:
 	#assuming 3 servos input
-	def __init__(self, s_x, s_y, s_za):
+	def __init__(self, s_x, s_y, s_z, distance):
 		self.servo_x = s_x
 		self.servo_y = s_y
 		self.servo_z = s_z
+		#distance of gimbal below the UAV
+		self.distance = distance
 
-	#set servo to rad change, unless hit limit, then
-	#reset to center because UAV made intentionally turn
-
-	#todo after get info on servo class
-	#z axis
-	def rotateGimbalZ(self, rad):
+	#expect input of vector
+	#set gimbal to point in direction of vector
+	def gimbal_set_vector(self, vector):
 		pass
+		
 
-	#x axis
-	def rotateGimbalX(self, rad):
-		pass
+	#input of 2 points, made out of list [x, y, z]
+	#set gimbal to point in that direction
+	def gimbal_point_in_space(self, UAV, point, quaternion):
+		vector = [0, 0, 0]
+		vector[0] = point[0] - UAV[0]
+		vector[1] = point[1] - UAV[1]
+		vector[2] = point[2] - UAV[2]
+		vector = qt.multiply(vector, quaternion)
+		gimbal_set_vector(vector)
 
-	#y axis
-	def rotateGimbalY(self, rad):
-		pass
-
-	'''
-	loop to automatically keep looking in same direction
-	unless direction changed by 1 of functions above.
-
-	angular is msgs.angular from a listener, from listerner
-	using "from geometry_msgs import Twist"
-	'''
-	def autoTrackingGimbal(self, angular):
-		#if changed, counteract change
-		if angular.x != 0:
-			rotateGimbalX(x)
-
-		if angular.y != 0:
-			rotateGimbalY(y)
-
-		if angular.z != 0:
-			rotateGimbalZ(z)
-
+	#return state of 3 servos in a list
+	def gimbal_get_state(self):
+		state = [0, 0, 0]
+		state[0] = self.servo_x.get_servo_angle
+		state[1] = self.servo_y.get_servo_angle
+		state[2] = self.servo_y.get_servo_angle
+		return state
+		
 
 
 
