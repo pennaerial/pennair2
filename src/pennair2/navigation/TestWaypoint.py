aaed6daa5d2e7c2d0120e3c6e5602964c546c55e
Maxwell Giancaterino <mgianc@seas.upenn.edu>
2017-11-12 12:51:47 -0500

    Finished unit tests for waypoint

--- src/pennair2/navigation/TestWaypoint.py
+++ src/pennair2/navigation/TestWaypoint.py
@@ -35,3 +35,36 @@ class TestWaypoint(unittest.TestCase):
         self.assertEqual(points.waypoints, [[2, 2, 2], [3, 3, 3]])
         points.delete(1)
         self.assertEqual(points.waypoints, [[2, 2, 2]])
+
+    def test_next(self):
+        points = waypoint.Waypoints("euclidean")
+        points.add([0, 0, 0])
+        points.add([1, 1, 1])
+        points.add([2, 2, 2])
+        self.assertEqual(points.next(), [0, 0, 0])
+        self.assertEqual(points.index, 1)
+        self.assertEqual(points.next(), [1, 1, 1])
+        self.assertEqual(points.index, 2)
+        self.assertEqual(points.next(), [2, 2, 2])
+        self.assertEqual(points.index, 0)
+
+    def test_goto(self):
+        points = waypoint.Waypoints("euclidean")
+        points.add([0, 0, 0])
+        points.add([1, 1, 1])
+        points.add([2, 2, 2])
+        points.add([3, 3, 3])
+        self.assertEqual(points.goto(3), [3, 3, 3])
+        self.assertEqual(points.index, 3)
+        self.assertEqual(points.goto(1), [1, 1, 1])
+        self.assertEqual(points.index, 1)
+
+    def test_length(self):
+        points = waypoint.Waypoints("euclidean")
+        self.assertEqual(points.length(), 0)
+        points.add([0, 0, 0])
+        self.assertEqual(points.length(), 1)
+        points.add([1, 1, 1])
+        points.add([2, 2, 2])
+        points.add([3, 3, 3])
+        self.assertEqual(points.length(), 4)
