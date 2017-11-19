91374818c4d23d29004663a8d70f792cffe9c3d4
Derek <dnong@sas.upenn.edu>
2017-11-19 12:04:56 -0800

    Untested takeoff

--- src/pennair2/core.py
+++ src/pennair2/core.py
@@ -1,6 +1,12 @@
 from .autopilot import Autopilot
 from abc import ABCMeta, abstractmethod
 
+import rospy
+from mavros_msgs.srv import CommandTOL
+from sensor_msgs.msg import NavSatFix
+from geometry_msgs.msg import TwistStamped, Twist, Vector3
+
+
 class UAV:
     def __init__(self, autopilot, metaclass=ABCMeta):
         """
@@ -23,8 +29,15 @@ class Multirotor(UAV):
         self._vel_setpoint = None
         self._acc_setpoint = None
 
-    def takeoff(self):
-        raise NotImplementedError
+
+        self._position = None
+
+
+    def takeoff(self, max_angle):
+        print("Attempting Takeoff")
+        msg = TwistStamped()
+        msg.Twist.linear = Vector3(0, 0, 0.5)
+        self.velocity = msg
 
     def land(self):
         raise NotImplementedError
@@ -34,16 +47,24 @@ class Multirotor(UAV):
 
     @property
     def position(self):
-        raise NotImplementedError
+        return self.autopilot.gps_raw
 
     @position.setter
-    def position(self):
-        raise NotImplementedError
+    def position(self, value):
+        try:
+            if type(value) is PoseStamped:
+                self.autopilot.velocity_pub.publish(value)
+            else:
+                print("Error while setting position: Value is not PoseStamped")
 
     @property
     def velocity(self):
-        raise NotImplementedError
+        return self.autopilot.gps_velocity_raw
 
     @velocity.setter
-    def velocity(self):
-        raise NotImplementedError
+    def velocity(self, value):
+        try:
+            if type(value) is TwistStamped:
+                self.autopilot.velocity_pub.publish(value)
+            else:
+                print("Error while setting velocity: Value is not TwistStamped")
