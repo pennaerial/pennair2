1b10a42c3189d294521c6a2ea5b2f5f19bdc1ae2
Stephen Eyerly <stepheneyerly@gmail.com>
2017-12-03 14:47:45 -0500

    implemented static map tile but needs some debugging

--- src/pennair2/gui.py
+++ src/pennair2/gui.py
@@ -9,7 +9,7 @@ try:
     from PIL import ImageTk
     from PIL import Image
     import googlemaps
-    from staticmap import StaticMap
+    from staticmap import StaticMap, CircleMarker
 except ImportError:
     exit()
 
@@ -17,6 +17,7 @@ except ImportError:
 class GroundStationApp:
     def __init__(self, master):
         self.master = master
+        self.render_image()
         self.initialize_image()
         self.create_altitude_widget()
         self.create_speed_widget()
@@ -45,11 +46,19 @@ class GroundStationApp:
         #This static map library will load map positions
         #self.map = StaticMap(width, height, padding_x, padding_y, url_template, tile_size)
 
+    # This method renders the images
+    def render_image(self):
+        self.static_map = StaticMap(width=300, height=300, url_template='http://a.tile.osm.org/{z}/{x}/{y}.png')
+        self.drone_marker = CircleMarker((39.952583, -75.165222), '#0036FF', 12)
+        self.static_map.add_marker(self.drone_marker)
+        image = self.static_map.render()
+        image.save('map.png')
+
     def run(self):
         self.master.mainloop()
 
 
-# The main method which runs the code
+# The main method which runs the code for testing purposes
 def main():
     root = Tk()
     app = GroundStationApp(root)
