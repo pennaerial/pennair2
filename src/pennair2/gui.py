39b0f97617a4e7468358eeeaf84e92f3d397c0af
Stephen Eyerly <stepheneyerly@gmail.com>
2018-01-09 11:39:14 -0500

    added in speed value and altitude value widgets

--- src/pennair2/gui.py
+++ src/pennair2/gui.py
@@ -16,44 +16,57 @@ except ImportError:
 
 
 class GroundStationApp:
-    def __init__(self, master):
+    def __init__(self, master, drone_latitude, drone_longitude):
         self.master = master
-        self.map_coordinates = (-75.165222, 39.952583)
+        self.map_coordinates = (drone_latitude, drone_longitude)
+        self.obstacles = []
+        self.altitude_value = 0
+        self.speed_value = 0
+        self.refresh_map()
+        self.create_altitude_widget()
+        self.create_altitude_value_widget()
+        self.create_speed_widget()
+        self.create_speed_value_widget()
+        self.create_speed_value_widget()
+
+    def refresh_map(self):
         self.render_image()
         self.initialize_image()
-        self.create_altitude_widget()
-        self.create_speed_widget()
-        # self.load_image()
 
     def initialize_image(self):
         #self.current_image_path = "map.png"
         #self.img = Image.open(self.current_image_path)
         self.photo_img = ImageTk.PhotoImage(self.image)
         self.panel = Label(self.master, image=self.photo_img)
-        self.panel.grid(row=1, column=0, columnspan=2)
+        self.panel.grid(row=1, column=0, columnspan=4)
 
     def create_altitude_widget(self):
-        self.altitude_widget = Label(self.master, text="Altitude")
+        self.altitude_widget = Label(self.master, text="Altitude:")
         self.altitude_widget.config(font=("Arial", 24))
         self.altitude_widget.grid(row=0, column=0)
 
-    def create_speed_widget(self):
-        self.altitude_widget = Label(self.master, text="Speed")
+    def create_altitude_value_widget(self):
+        self.altitude_widget = Label(self.master, text=str(self.altitude_value))
         self.altitude_widget.config(font=("Arial", 24))
         self.altitude_widget.grid(row=0, column=1)
 
-    def load_image(self):
-        self.map = None
-        #This static map library will load map positions
-        #self.map = StaticMap(width, height, padding_x, padding_y, url_template, tile_size)
+    def create_speed_widget(self):
+        self.altitude_widget = Label(self.master, text="Speed:")
+        self.altitude_widget.config(font=("Arial", 24))
+        self.altitude_widget.grid(row=0, column=2)
+
+    def create_speed_value_widget(self):
+        self.altitude_widget = Label(self.master, text=str(self.speed_value))
+        self.altitude_widget.config(font=("Arial", 24))
+        self.altitude_widget.grid(row=0, column=3)
+
 
     # This method renders the images
     def render_image(self):
         self.static_map = StaticMap(700, 700)#, url_template='http://a.tile.osm.org/{z}/{x}/{y}.png')
         self.drone_marker = CircleMarker(self.map_coordinates, '#0036FF', 12)
         self.static_map.add_marker(self.drone_marker)
-        self.image = self.static_map.render(zoom=12)
-        #self.image.save('map.png')
+        self.image = self.static_map.render(zoom=17)
 
     def run(self):
         self.master.mainloop()
@@ -62,7 +75,7 @@ class GroundStationApp:
 # The main method which runs the code for testing purposes
 def main():
     root = Tk()
-    app = GroundStationApp(root)
+    app = GroundStationApp(root, -75.165222, 39.952583)
     app.run()
 
 
