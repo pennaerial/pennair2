e29ef0d7b9733ad271cf78869389031009693b62
Stephen Eyerly <stepheneyerly@gmail.com>
2018-02-06 18:22:38 -0500

    got multiple markers to work

--- src/pennair2/gui.py
+++ src/pennair2/gui.py
@@ -32,23 +32,27 @@ class GroundStationApp:
 
     def add_boundary(self, start_long, start_lat, end_long, end_lat):
         self.boundaries.append(Line(((start_long, start_lat), (end_long, end_lat)), 'blue', 3))
-        self.panel.grid(row=1, column=0, columnspan=4)
+        self.refresh_map()
+        self.master.update()
 
     def add_obstacle(self, long, lat):
         self.obstacles.append(CircleMarker((long, lat), 'red', 12))
-
-    def create_altitude_widget(self):
-        self.altitude_widget = Label(self.master, text="Altitude:")
+        self.refresh_map()
+        self.master.update()
 
     def refresh_map(self):
         self.render_image()
-        self.initialize_image()
+        self.display_image()
 
-    def initialize_image(self):
-        #self.current_image_path = "map.png"
-        #self.img = Image.open(self.current_image_path)
-        self.photo_img = ImageTk.PhotoImage(self.image)
-        self.panel = Label(self.master, image=self.photo_img)
+    def display_image(self):
+        self.current_image_path = "map.png"
+        self.img = Image.open(self.current_image_path)
+        self.photo_img = ImageTk.PhotoImage(self.img)
+        self.map_widget = Label(self.master, image=self.photo_img)
+        self.map_widget.grid(row=1, column=0, columnspan=4)
+
+    def create_altitude_widget(self):
+        self.altitude_widget = Label(self.master, text="Altitude:")
         self.altitude_widget.config(font=("Arial", 24))
         self.altitude_widget.grid(row=0, column=0)
 
@@ -73,7 +77,12 @@ class GroundStationApp:
         self.static_map = StaticMap(700, 700)#, url_template='http://a.tile.osm.org/{z}/{x}/{y}.png')
         self.drone_marker = CircleMarker(self.map_coordinates, '#0036FF', 12)
         self.static_map.add_marker(self.drone_marker)
+        for obstacle in self.obstacles:
+            self.static_map.add_marker(obstacle)
+            print("added it")
+
         self.image = self.static_map.render(zoom=17)
+        self.image.save('map.png')
 
     def run(self):
         self.master.mainloop()
@@ -83,7 +92,8 @@ class GroundStationApp:
 def main():
     root = Tk()
     app = GroundStationApp(root, -75.165222, 39.952583)
-    app.add_boundary(-75.165200, 39.952500, -75.165300, 39.952600)
+    app.add_boundary(-75.164000, 39.951000, -75.165300, 39.952600)
+    app.add_obstacle(-75.164220, 39.9526585)
     app.run()
 
 
