b210f8475983e050314521d4295c1e97bc8388a5
Damian Owerko <Damowerko@users.noreply.github.com>
2018-02-14 13:14:21 -0500

    Update launch.py

--- src/pennair2/launch.py
+++ src/pennair2/launch.py
@@ -1,3 +1,6 @@
+# Copyright (C) 2018  Penn Aerial Robotics
+# Fill copyright notice at github.com/pennaerial/pennair2/NOTICE
+
 from xml.etree.ElementTree import Element, SubElement, tostring
 from xml.dom import minidom
 import rospkg
