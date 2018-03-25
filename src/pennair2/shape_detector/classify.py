b16e705bf8379354c65e497d6fa3baf764144c36
yimengsu <yimengsu@gmail.com>
2018-03-24 21:44:31 -0400

    removing what got moved to osprey

--- src/pennair2/shape_detector/classify.py
+++
@@ -1,8 +0,0 @@
-import sys
-import stratify
-
-# input: name of file, color to find, colors to ignore (default none)
-if len(sys.argv) > 2:
-	stratify.stratify(sys.argv[1], sys.argv[2], sys.argv[3:])
-elif len(sys.argv) > 1:
-	stratify.stratify(sys.argv[1], sys.argv[2])
