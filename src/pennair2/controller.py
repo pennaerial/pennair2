ceb348a42a4225b74f62f6484c6b01264f251bc1
Damian Owerko <damowerko@gmail.com>
2018-05-12 18:58:20 -0700

    Moved controller.py to pennair2.

---
+++ src/pennair2/controller.py
@@ -0,0 +1,15 @@
+from threading import Thread
+from abc import ABCMeta, abstractmethod
+from pennair2.core import Multirotor
+
+class Controller(Thread):
+    __metaclass__ = ABCMeta
+
+    def __init__(self, quad):
+        # type: (Multirotor) -> None
+        self.quad = quad  # type: Multirotor
+        Thread.__init__(self)
+
+    @abstractmethod
+    def run(self):
+        pass
