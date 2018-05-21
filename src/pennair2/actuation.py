23e10aef937b471fc99f60f7f8e7888a2bd90972
Damian Owerko <damowerko@gmail.com>
2018-05-21 13:12:33 -0400

    Added RCOverride class to actuation

--- src/pennair2/actuation.py
+++ src/pennair2/actuation.py
@@ -1,2 +1,19 @@
 # Copyright (C) 2018  Penn Aerial Robotics
 # Fill copyright notice at github.com/pennaerial/pennair2/NOTICE
+
+import rospy
+from mavros_msgs.msg import OverrideRCIn
+
+
+class RCOverride:
+    def __init__(self, mavros_prefix="/mavros"):
+        # type: (str) -> None
+        self.actuator_pub = rospy.Publisher(mavros_prefix + "/rc/override", OverrideRCIn, queue_size=10)
+        self.channels = [OverrideRCIn.CHAN_RELEASE] * 8
+
+    def set_pwm(self, val, channel):
+        # type: (int, int) -> None
+        msg = OverrideRCIn()  # type: OverrideRCIn
+        self.channels[channel] = val
+        msg.channels = channel
+        self.actuator_pub.publish(msg)
