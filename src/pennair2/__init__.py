a035e2155f6568b6023e8d1ec95ed3f8a7570336
Damian Owerko <damowerko@gmail.com>
2018-05-21 16:13:42 -0400

    Localization moved

--- src/pennair2/__init__.py
+++ src/pennair2/__init__.py
@@ -1,5 +1,4 @@
 __all__ = ['actuation', 'autopilot', 'controller', 'conversions', 'core', 'gui', 'launch', 'swarm', 'search', 'PID', 'navigation', 'vision']
-from .core import UAV, Multirotor
-from .autopilot import Autopilot
-from .controller import Controller
-
+from core import UAV, Multirotor
+from autopilot import Autopilot
+from controller import Controller
