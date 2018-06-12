4625c6fe97b50cf171f231a76376522234322b59
Maxwell Giancaterino <mgianc@seas.upenn.edu>
2018-06-12 19:41:05 -0400

    changed coordinate system to Euclidean

--- src/pennair2/vision/orthorectify.py
+++ src/pennair2/vision/orthorectify.py
@@ -1,28 +1,18 @@
 import math
-import geopy
-import geopy.distance
 
 #helper method to find one dimensional displacement
 def displacement(pixel_coord, pixel_length, FOV, camera_tilt, altitude):
-    target_angle = pixel_coord / pixel_length * FOV - FOV / 2 - camera_tilt;
+    target_angle = pixel_coord / pixel_length * FOV - FOV / 2 + camera_tilt;
     return altitude * math.tan(target_angle)
 #print(displacement(1, 4, 0.927295,- math.pi / 4, 6))
 
-#shift latitude or longitude by certain number of meters
-def shift_coord(lat, lon, dlat, dlon):
-    shift_lat = geopy.distance.VincentyDistance(kilometers = dlat)
-    shift_lon = geopy.distance.VincentyDistance(kilometers = dlon)
-    end_lat = shift_lat.destination(point=geopy.Point(lat, lon), bearing = 90)
-    return shift_lon.destination(point=end_lat, bearing = 0)
-#print(shift_coord(48.853, 2.349, 0, 1))
-
 #find coordinates of location on a picture and returns them as a tuple
-def get_coord(lat, lon, x_pixel, y_pixel, pic_width, pic_height, hor_fov, ver_fov, roll, pitch, yaw, altitude):
+def get_coord(x_drone, y_drone, x_pixel, y_pixel, pic_width, pic_height, hor_fov, ver_fov, roll, pitch, yaw, altitude):
     """
-    :param lat: latitude at time of picture taken in degrees
+    :param x_drone: x-position of drone
     :type lat: float
 
-    :param lon: longitude at time of picture taken in degrees
+    :param y_drone: y-position of drone
     :type lon: float
 
     :param x_pixel: x coordinate of pixel of target
@@ -55,10 +45,7 @@ def get_coord(lat, lon, x_pixel, y_pixel, pic_width, pic_height, hor_fov, ver_fo
     :param altitude: altitude in kilometers
     :type altitude: float
     """
-    dx = displacement(x_pixel, pic_width, hor_fov, roll, altitude)
-    dy = displacement(y_pixel, pic_height, ver_fov, -pitch, altitude)
-    dlat = dy * math.sin(yaw) + dx * math.cos(yaw)
-    dlon = dy * math.cos(yaw) + dx * math.sin(yaw)
-    tgt_coord = shift_coord(lat, lon, dlat, dlon)
-    return (tgt_coord.latitude, tgt_coord.longitude)
-#print(get_coord(48.853, 2.349, 5, 100, 100, 200, math.pi / 2, math.pi / 3, 0, 0, 180, 10))
+    dx = -displacement(y_pixel, pic_height, ver_fov, -pitch, altitude)
+    dy = displacement(x_pixel, pic_width, hor_fov, roll, altitude)
+    return (x_drone + dx, y_drone + dy)
+print(get_coord(48.853, 2.349, 5, 100, 100, 200, math.pi / 2, math.pi / 3, 0, 0, 180, 10))
