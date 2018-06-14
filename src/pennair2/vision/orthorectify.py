4b48cda2a932250a4ed22deb9b86e977eeea0ec5
Maxwell Giancaterino <mgianc@seas.upenn.edu>
2018-06-13 23:27:30 -0400

    calculated euler angles from quaternion

--- src/pennair2/vision/orthorectify.py
+++ src/pennair2/vision/orthorectify.py
@@ -9,22 +9,8 @@ def displacement(pixel_coord, pixel_length, FOV, camera_tilt, altitude):
     return altitude * math.tan(target_angle)
 #print(displacement(1, 4, 0.927295,- math.pi / 4, 6))
 
-def qv_mult(q1, v1):
-    v1 = transformations.unit_vector(v1)
-    q2 = list(v1)
-    q2.append(0.0)
-    return transformations.quaternion_multiply(
-        transformations.quaternion_multiply(q1, q2),
-        transformations.quaternion_conjugate(q1))[:3]
-
-def relative_displacement(pose, x_pixel, y_pixel, img_width, img_height, hor_fox, ver_fov):
-    # type: (Pose) -> np.ndarray
-    direction = [0, 0, -1]
-    q = [pose.orientation.x, pose.orientation.y, pose.orientation.z, pose.orientation.w]
-    direction = qv_mult(q, direction)
-
 #find coordinates of location on a picture and returns them as a tuple
-def get_coord(x_drone, y_drone, x_pixel, y_pixel, pic_width, pic_height, hor_fov, ver_fov, roll, pitch, yaw, altitude):
+def get_coord(pose, x_drone, y_drone, x_pixel, y_pixel, pic_width, pic_height, hor_fov, ver_fov, altitude):
     """
     :param x_drone: x-position of drone
     :type lat: float
@@ -62,7 +48,8 @@ def get_coord(x_drone, y_drone, x_pixel, y_pixel, pic_width, pic_height, hor_fov
     :param altitude: altitude in kilometers
     :type altitude: float
     """
-    dx = -displacement(y_pixel, pic_height, ver_fov, -pitch, altitude)
+    quaternion = [pose.orientation.x, pose.orientation.y, pose.orientation.z, pose.orientation.w]
+    (roll, pitch, yaw) = transformations.euler_from_quaternion(quaternion)
+    dx = displacement(y_pixel, pic_height, ver_fov, pitch, altitude)
     dy = displacement(x_pixel, pic_width, hor_fov, roll, altitude)
-    return (x_drone + dx, y_drone + dy)
-print(get_coord(48.853, 2.349, 5, 100, 100, 200, math.pi / 2, math.pi / 3, 0, 0, 180, 10))
+    return (x + dx, y + dy, altitude)
