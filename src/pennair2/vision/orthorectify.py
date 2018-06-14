0719c3b60576d580dd3a0a1885ec91f6ccfd1904
Maxwell Giancaterino <mgianc@seas.upenn.edu>
2018-06-13 23:59:17 -0400

    added quaternion multiplication

--- src/pennair2/vision/orthorectify.py
+++ src/pennair2/vision/orthorectify.py
@@ -3,14 +3,17 @@ from geometry_msgs.msg import Pose
 import numpy as np
 from tf import transformations
 
-#helper method to find one dimensional displacement
-def displacement(pixel_coord, pixel_length, FOV, camera_tilt, altitude):
-    target_angle = pixel_coord / pixel_length * FOV - FOV / 2 + camera_tilt;
-    return altitude * math.tan(target_angle)
 #print(displacement(1, 4, 0.927295,- math.pi / 4, 6))
+def qv_mult(q1, v1):
+    v1 = transformations.unit_vector(v1)
+    q2 = list(v1)
+    q2.append(0.0)
+    return transformations.quaternion_multiply(
+        transformations.quaternion_multiply(q1, q2),
+        transformations.quaternion_conjugate(q1))[:3]
 
 #find coordinates of location on a picture and returns them as a tuple
-def get_coord(pose, x_drone, y_drone, x_pixel, y_pixel, pic_width, pic_height, hor_fov, ver_fov, altitude):
+def get_coord(pose, x_pixel, y_pixel, pic_width, pic_height, hor_fov, ver_fov):
     """
     :param x_drone: x-position of drone
     :type lat: float
@@ -48,8 +51,11 @@ def get_coord(pose, x_drone, y_drone, x_pixel, y_pixel, pic_width, pic_height, h
     :param altitude: altitude in kilometers
     :type altitude: float
     """
-    quaternion = [pose.orientation.x, pose.orientation.y, pose.orientation.z, pose.orientation.w]
-    (roll, pitch, yaw) = transformations.euler_from_quaternion(quaternion)
-    dx = displacement(y_pixel, pic_height, ver_fov, pitch, altitude)
-    dy = displacement(x_pixel, pic_width, hor_fov, roll, altitude)
-    return (x + dx, y + dy, altitude)
+    roll = x_pixel/pic_width * hor_fov - hor_fov / 2
+    pitch = y_pixel / pic_height * ver_fov - ver_fov / 2
+    q1 = transformations.quaternion_from_euler(roll, pitch, 0, axes='srpy')
+    q2 = [pose.orientation.x, pose.orientation.y, pose.orientation.z, pose.orientation.w]
+    q_total = transformations.quaternion_multiply(q2, q1)
+    direction = [0, 0, -1]
+    (dx, dy, dz) = qv_mult(q_total, direction)
+    return pose.position.x + dx, pose.position.y + dy, pose.position.z
