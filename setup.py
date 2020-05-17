757fd11c6b26eb592c2cc4f930f54c54c4b0a5d3
Damian Owerko <damowerko@gmail.com>
2020-05-17 17:48:46 -0400

    fix typo in setup

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 # fetch values from package.xml
 setup_args = generate_distutils_setup(
         packages=['pennair2','pennair2.navigation','pennair2.vision'],
-        package_dir={'': 'src'},
+        package_dir={'': 'src'}
         )
 
 setup(**setup_args)
